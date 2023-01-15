32c44cce985ec1b56d9be7951c40a53589141ca4
insanz01 <insankamil002@gmail.com>
2023-01-15 13:18:44 +0700

    rename membership into karyawan

--- application/views/app/membership/index.php
+++ application/views/app/membership/index.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0 text-dark">Membership</h1>
+          <h1 class="m-0 text-dark">Karyawan</h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <!-- <ol class="breadcrumb float-sm-right">
@@ -25,7 +25,7 @@
         <div class="col-12">
           <div class="row">
             <div class="col-12">
-              <a href="<?= base_url('membership/add') ?>" class="btn btn-primary float-right mb-2">TAMBAH MEMBER</a>
+              <a href="<?= base_url('membership/add') ?>" class="btn btn-primary float-right mb-2">TAMBAH KARYAWAN</a>
             </div>
             <div class="col-12">
               <div class="card">
@@ -33,7 +33,7 @@
                   <table class="table custom-table">
                     <thead>
                       <th>#</th>
-                      <th>Kode Member</th>
+                      <th>Kode Karyawan</th>
                       <th>Nama</th>
                       <th>Alamat</th>
                       <th>Email</th>
